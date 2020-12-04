5292031f1db3e7dc3cc69ecd78c51e369905b9c4
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-12-04 18:14:28 +0300

    sets up webpack in the project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./css/index.css"
+console.log('I\'m working')
+console.log("again it's me")
+console.log("yes it;s true")
+const elem = document.createElement('nav')
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8080,
+  },
+
+};
