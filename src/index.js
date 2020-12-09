cd39bd9de75c9d519711123c7a4f0a7b84b7e904
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-12-10 00:35:01 +0300

    Adds home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import 'bootstrap';
-import './scss/index.scss'
-console.log('I\'m working')
-console.log("again it's me")
-console.log("yes it;s true")
-const elem = document.createElement('nav')
+import './scss/index.scss';
+import HomePage from '../src/pages/home';
+
+const elem = document.getElementById('content');
+elem.appendChild(HomePage());
+
+// elem.innerHTML="";
+
+// elem.removeChild(elem.lastElementChild)
